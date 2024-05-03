2504a9a7941c132d3f3fe257bdb9a1fc5b947c30
Marien Giordani <marien.giordani@student.isae-supaero.fr>
2024-05-03 15:30:57 +0200

    Fixed an import bug

--- reservations/reservationsapp/forms.py
+++ reservations/reservationsapp/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Gare, Reservation, Trajet, Passager, Client
+from .models import Gare, Reservation, Journey, Passager, Client
 from django.forms import ModelForm, inlineformset_factory
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
