a1758366259244d21dc5757794d6c79b8e4c13f0
adrien7779 <adrien7779@outlook.com>
2024-05-07 01:40:37 +0200

    Pushing w Adrien modifs

--- reservations/reservations/settings.py
+++ reservations/reservations/settings.py
@@ -97,9 +97,9 @@ LANGUAGE_CODE = "fr-fr"
 
 TIME_ZONE = "Europe/Paris"
 
-USE_I18N = True
+USE_I18N = False
 
-USE_TZ = False #was True :: If USE_TZ is True, Django will store the datetime information with UTC time zone, and if it is False, it will store it with the local time zone
+USE_TZ = True #was True :: If USE_TZ is True, Django will store the datetime information with UTC time zone, and if it is False, it will store it with the local time zone
 
 
 # Static files (CSS, JavaScript, Images)
